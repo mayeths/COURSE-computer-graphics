280501a944eaf77f06fa263054e6b0a63d19c56f
mayeths <wow@mayeths.com>
2022-11-07 18:26:27 +0800

    LearnOpenGL 6.3

--- C-3-pyramid/src/main.cpp
+++ C-3-pyramid/src/main.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #include <array>
+#include <vector>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/random.hpp>
@@ -188,22 +189,41 @@ int main() {
             glActiveTexture(GL_TEXTURE1);
             glBindTexture(GL_TEXTURE_2D, texture2);
 
-            glm::mat4 model = glm::mat4(1.0f);
-            glm::mat4 view = glm::mat4(1.0f);
-            glm::mat4 projection = glm::mat4(1.0f);
-            model = glm::rotate(model, (float)now, glm::vec3(0.5f, 1.0f, 0.0f));
-            view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-            projection = glm::perspective(glm::radians(45.0f), (float)window.SCR_WIDTH / (float)window.SCR_HEIGHT, 0.1f, 100.0f);
+            const std::vector<glm::vec3> cubePositions {
+                glm::vec3( 0.0f,  0.0f,  0.0f),
+                glm::vec3( 2.0f,  5.0f, -15.0f),
+                glm::vec3(-1.5f, -2.2f, -2.5f),
+                glm::vec3(-3.8f, -2.0f, -12.3f),
+                glm::vec3( 2.4f, -0.4f, -3.5f),
+                glm::vec3(-1.7f,  3.0f, -7.5f),
+                glm::vec3( 1.3f, -2.0f, -2.5f),
+                glm::vec3( 1.5f,  2.0f, -2.5f),
+                glm::vec3( 1.5f,  0.2f, -1.5f),
+                glm::vec3(-1.3f,  1.0f, -1.5f),
+            };
 
             ourShader.use();
-            GLuint modelLoc = glGetUniformLocation(ourShader.ID, "model");
-            GLuint viewLoc = glGetUniformLocation(ourShader.ID, "view");
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-            glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-            ourShader.setMat4("projection", projection);
-
             glBindVertexArray(VAO);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
+            for (int i = 0; i < cubePositions.size(); i++) {
+                glm::mat4 model = glm::mat4(1.0f);
+                model = glm::translate(model, cubePositions[i]);
+                model = glm::rotate(model, (float)(20.0f * i + now), glm::vec3(1.0f, 0.3f, 0.5f));
+
+                glm::mat4 view = glm::mat4(1.0f);
+                view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+                glm::mat4 projection = glm::mat4(1.0f);
+                projection = glm::perspective(glm::radians(45.0f), (float)window.SCR_WIDTH / (float)window.SCR_HEIGHT, 0.1f, 100.0f);
+
+                GLuint modelLoc = glGetUniformLocation(ourShader.ID, "model");
+                GLuint viewLoc = glGetUniformLocation(ourShader.ID, "view");
+                glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+                glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+                ourShader.setMat4("projection", projection);
+
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+            }
+
             // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
             gui.update();
