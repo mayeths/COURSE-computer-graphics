f058a60c9f71513793e08892281a935adba99460
mayeths <wow@mayeths.com>
2022-12-06 17:14:24 +0800

    Move assets localtions

--- A-6-TerrianEngine/src/main.cpp
+++ A-6-TerrianEngine/src/main.cpp
@@ -34,7 +34,7 @@ int main() {
 
     std::vector<SmileBox> smileBoxs(12);
     for (int i = 0; i < smileBoxs.size(); i++) {
-        smileBoxs[i].SetShaderPath("assets/6.2.coordinate_systems.vs", "assets/6.2.coordinate_systems.fs");
+        smileBoxs[i].SetShaderPath("assets/shaders/smilebox.vs", "assets/shaders/smilebox.fs");
         smileBoxs[i].SetTexturePath("assets/textures/container.jpg", "assets/textures/awesomeface.png");
         smileBoxs[i].Setup(i);
 
@@ -65,13 +65,13 @@ int main() {
     Scene scene;
     SkyBox skybox;
     {
-        skybox.SetShaderPath("assets/skybox.vs", "assets/skybox.fs");
-        skybox.SetTopImagePath("assets/SkyBox/SkyBox4.bmp");
-        skybox.SetNorthImagePath("assets/SkyBox/SkyBox0.bmp");
-        skybox.SetEastImagePath("assets/SkyBox/SkyBox1.bmp", -90);
-        skybox.SetSouthImagePath("assets/SkyBox/SkyBox2.bmp");
-        skybox.SetWestImagePath("assets/SkyBox/SkyBox3.bmp", -90);
-        skybox.SetBottomImagePath("assets/SkyBox/SkyBox5.bmp");
+        skybox.SetShaderPath("assets/shaders/skybox.vs", "assets/shaders/skybox.fs");
+        skybox.SetTopImagePath("assets/textures/SkyBox4.bmp");
+        skybox.SetNorthImagePath("assets/textures/SkyBox0.bmp");
+        skybox.SetEastImagePath("assets/textures/SkyBox1.bmp", -90);
+        skybox.SetSouthImagePath("assets/textures/SkyBox2.bmp");
+        skybox.SetWestImagePath("assets/textures/SkyBox3.bmp", -90);
+        skybox.SetBottomImagePath("assets/textures/SkyBox5.bmp");
         GLfloat skyBoxWidth = 1e4f; // Big value (likes 1e5f) will meet precision limit when testing depth
         skybox.SetBoxWidth(skyBoxWidth);
         skybox.MoveWith(glm::vec3(0.0f, skyBoxWidth/2, 0.0f));
@@ -81,12 +81,12 @@ int main() {
     Terrian terrian;
     {
         terrian.SetShaderPath(
-            "assets/8.3.gpuheight.vs", "assets/8.3.gpuheight.fs",
-            "assets/8.3.gpuheight.tcs", "assets/8.3.gpuheight.tes"
+            "assets/shaders/terrian.vs", "assets/shaders/terrian.fs",
+            "assets/shaders/terrian.tcs", "assets/shaders/terrian.tes"
         );
-        terrian.SetHeightMapPath("assets/TerrianHW/heightmap.bmp");
-        terrian.SetTexturePath("assets/TerrianHW/terrain-texture3.bmp");
-        terrian.SetDetailTexturePath("assets/TerrianHW/detail.bmp");
+        terrian.SetHeightMapPath("assets/textures/heightmap.bmp");
+        terrian.SetTexturePath("assets/textures/terrain-texture3.bmp");
+        terrian.SetDetailTexturePath("assets/textures/detail.bmp");
         terrian.MoveWith(glm::vec3(0.0f, -2.0f, 0.0f));
         terrian.Setup();
     }
